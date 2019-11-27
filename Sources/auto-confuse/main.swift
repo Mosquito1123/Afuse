2aa345f5c78e9674fd4c35e343dd36a04c6f8de2
Winston Zhang <winston.zhangwentong@gmail.com>
2019-11-27 17:18:30 +0800

    ADD:基本参数格式化

--- Sources/auto-confuse/main.swift
+++ Sources/auto-confuse/main.swift
@@ -3,25 +3,38 @@ import CommandLineKit
 
 let cli = CommandLineKit.CommandLine()
 
-let filePath = StringOption(shortFlag: "f", longFlag: "file", required: true,
-  helpMessage: "Path to the output file.")
-let compress = BoolOption(shortFlag: "c", longFlag: "compress",
-  helpMessage: "Use data compression.")
 let help = BoolOption(shortFlag: "h", longFlag: "help",
   helpMessage: "Prints a help message.")
 let verbosity = CounterOption(shortFlag: "v", longFlag: "verbose",
   helpMessage: "Print verbose messages. Specify multiple times to increase verbosity.")
 
-cli.addOptions(filePath, compress, help, verbosity)
 
+
+
+
+let filePathx = StringOption(shortFlag: "i", longFlag: "input", required: true,
+  helpMessage: "Path to the input file.")
+let spamCodeOutPathx = StringOption(shortFlag: "s", longFlag: "spamCodeOut",
+  helpMessage: "Generate spam code output path.")
+let handleXcassetsx = BoolOption(shortFlag: "x", longFlag: "handleXcassets",
+helpMessage: "Handle xcassets confuse.")
+let deleteCommentsx = BoolOption(shortFlag: "d", longFlag: "deleteComments",
+helpMessage: "Delete comments.")
+let modifyProjectNamex = StringOption(shortFlag: "m", longFlag: "modifyProjectName",
+helpMessage: "Modify ProjectName.Format:OldName>NewName.For example:DDApp>CCApp.")
+let modifyClassNamePrefixx = StringOption(shortFlag: "p", longFlag: "modifyClassNamePrefix",
+helpMessage: "Modify ClassNamePrefix.Format:OldClassNamePrefix>NewClassNamePrefix.For example:DDApp>CCApp.")
+let ignoreDirNamesx = StringOption(shortFlag: "g", longFlag: "ignoreDirNames",
+helpMessage: "Ignore dirNames.Format:A,B,C....For example:DDApp,CCApp,BBApp....")
+cli.addOptions(filePathx,spamCodeOutPathx,handleXcassetsx,deleteCommentsx,modifyProjectNamex,modifyClassNamePrefixx,ignoreDirNamesx)
 do {
   try cli.parse()
 } catch let error{
   cli.printUsage(error)
 }
 
-print("File path is \(filePath.value ?? "")")
-print("Compress is \(compress.value)")
+print("File path is \(filePathx.value ?? "")")
+//print("Compress is \(compress.value)")
 print("Verbosity is \(verbosity.value)")
 
 //void recursiveDirectory(NSString *directory, NSArray<NSString *> *ignoreDirNames, void(^handleMFile)(NSString *mFilePath), void(^handleSwiftFile)(NSString *swiftFilePath));
